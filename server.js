e858cdce8ec7df0108c7342370d0373951152a31
Leaveil <leaveil.armstrong@gmail.com>
2018-02-09 20:42:28 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+const path = require('path');
+const articlesController = require("./controllers/articlescontroller");
+const router = express.Router();
+const db = require('./models');
+
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Serve up static assets
+
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/articles', (req, res) => {
+	db.Article
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+})
+
+// const routes = () => {router
+// 		.route('/api/articles/')
+// 		.get(articlesController.findAll)
+// 		.post(articlesController.create);
+// 	}
+
+// app.use(routes)
+
+
+router
+	.route('*')
+	.get((req, res) => {
+		res.sendFile(path.join(__dirname + '/client/build/index.html'));
+	})
+
+const PORT = process.env.PORT || 8080;
+
+// Set up promises with mongoose
+mongoose.Promise = global.Promise;
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/nytreact"
+);
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+	// console.log(path.join(__dirname, "../client/build/index.html"))
+});
